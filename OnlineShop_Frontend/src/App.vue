6547e08698dad866499f3b8848525d34e5e5b8ab
HoadreaRazvan <162281824+HoadreaRazvan@users.noreply.github.com>
2024-07-05 15:05:36 +0300

    Initializare

---
+++ OnlineShop_Frontend/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+let message = ref('Hello World');
+let color = ref('black');
+function changeColor() {
+    color.value = 'red';
+    message.value = 'Salut';
+}
+
+</script>
+
+<template>
+
+<button @click="changeColor" :style="{ color }">{{ message }}</button> 
+
+</template>
+
+<style scoped>
+
+button {
+    font-size: 20px;
+    background-color: lightblue;
+}
+
+</style>
