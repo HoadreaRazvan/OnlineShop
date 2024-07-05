52c3d9c8bcc3004bfa20fa77b31cc9c4b79d7749
HoadreaRazvan <162281824+HoadreaRazvan@users.noreply.github.com>
2024-07-06 00:58:10 +0300

    Practice

--- OnlineShop_Frontend/src/App.vue
+++ OnlineShop_Frontend/src/App.vue
@@ -1,26 +1,35 @@
-<script setup lang="ts">
+<script setup>
+import { ref } from 'vue'
 
-import { ref } from 'vue';
-let message = ref('Hello World');
-let color = ref('black');
-function changeColor() {
-    color.value = 'red';
-    message.value = 'Salut';
+// give each todo a unique id
+let id = 0
+
+const newTodo = ref('')
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' }
+])
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
 }
 
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
 </script>
 
 <template>
-
-<button @click="changeColor" :style="{ color }">{{ message }}</button> 
-
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
-
-<style scoped>
-
-button {
-    font-size: 20px;
-    background-color: lightblue;
-}
-
-</style>
